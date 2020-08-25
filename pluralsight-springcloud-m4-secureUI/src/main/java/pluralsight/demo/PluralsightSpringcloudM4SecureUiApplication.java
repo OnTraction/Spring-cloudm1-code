f3135469d0ece8b1e7e0b9b2465906649992b7fc
Val <59769136+ValMongina@users.noreply.github.com>
2020-08-25 11:56:12 -0700

    Initial Commit1
    1st

---
+++ pluralsight-springcloud-m4-secureUI/src/main/java/pluralsight/demo/PluralsightSpringcloudM4SecureUiApplication.java
@@ -0,0 +1,13 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightSpringcloudM4SecureUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM4SecureUiApplication.class, args);
+	}
+
+}
